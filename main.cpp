37601fac7a617723f519301fb5b3a74d2f399026
Ashad001 <93534298+Ashad001@users.noreply.github.com>
2022-11-12 21:10:00 +0500

    Tree Class v-01

--- main.cpp
+++ main.cpp
@@ -1,30 +1,48 @@
 #include <iostream>
-#include <vector>
 #include <map>
 #include <string>
+#include <vector>
 using namespace std;
 
 class TreeNode
 {
 public:
-    int index;
-    int value;
-    int depth;
-    int score;
 
-    TreeNode *left;
-    TreeNode *right;
+    // 1. int column value.. refers to column split on by parent node
+    //    null in root's case
+    int ColVal;
+    // 2. int column split on.. column this node splits on
+    //    value doesn't matter if guess is not null
+    int ColSplitOn;
+    // 3. int guess
+    //    null unless node is a leaf case
+    int Guess; // On leaf!
+    // 4. vector of node pointers called "children"
+    //    pointers to all of the children of this node
+    //    also doesn't matter if node is base case
+    vector<TreeNode *> children;
+    // constructor
     TreeNode()
     {
-        index = value = depth = score = 0;
-        left = right = NULL;
+        ColVal = ColSplitOn = Guess = NULL;
     }
 };
+class DecisionTree
+{
+public:
+    TreeNode *root;
+    vector<vector<int>> Data;
+    vector<vector<int>> colVals;
+    DecisionTree()
+    {
+        // This Constructor Will Fetch The Data -- OVER @HASEEB   
+    }
+    
+};
+
 void Fit(vector<vector<int>> matrix, vector<vector<int>> label)
 {
-
 }
-
 
 int main(int argc, char const *argv[])
 {
