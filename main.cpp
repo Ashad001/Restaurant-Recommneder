c36848d3fa3410a76d516d2725a918c50d101c5f
Ashad001 <93534298+Ashad001@users.noreply.github.com>
2022-11-14 22:45:21 +0500

    updated main

--- main.cpp
+++ main.cpp
@@ -57,53 +57,43 @@ vector<vector<string>> RETRIEVE_DATA(string filename)
     }
     return record;
 }
-bool alreadyExist(string c, vector<string> ch)
+vector<vector<int>> Bit_Mask(vector<vector<string>> &data)
 {
-    for (auto x : ch)
+    // Converting string data to integers (AKA masking)
+    vector<vector<int>> cvt(data.size(), vector<int>(data[0].size() - 2, 0));
+    int index = 2; // First Two Cols not necessary here
+    int row = 0, col = 0;
+    map<string, int> mp;
+
+    for (index = 2; index < data[0].size(); index += 1)
     {
-        if (x == c)
+        row = 0;
+        mp.clear();
+        for (int i = 0; i < data.size(); i++)
         {
-            return true;
+            mp[data[i][index]]++;
         }
+        for (int i = 0; i < data.size(); i++)
+        {
+            cvt[row][col] = mp[data[i][index]] % 4;
+            row++;
+        }
+        col++;
     }
-    return false;
+    return cvt;
 }
 
 class DecisionTree
 {
 public:
     TreeNode *root;
-    vector<vector<string>> Data;
-    vector<vector<int>> colVals;
+    vector<vector<int>> Data;
+    // vector<vector<int>> colVals;
     DecisionTree(string filename)
     {
         // This Constructor Will Fetch The Data -- OVER @HASEEB
-        this->Data = RETRIEVE_DATA(filename);
-    }
-    vector<vector<int>> Bit_Mask(vector<vector<string>> &data)
-    {
-        // Converting string data to integers (AKA masking)
-        vector<vector<int>> cvt(data.size(), vector<int>(data[0].size() - 2, 0));
-        int index = 2;             // First Two Cols not necessary here 
-        int row = 0, col = 0;
-        map<string, int> mp;
-
-        for (index = 2; index < data[0].size(); index += 1)
-        {
-            row = 0;
-            mp.clear();
-            for (int i = 0; i < data.size(); i++)
-            {
-                mp[data[i][index]]++;
-            }
-            for (int i = 0; i < data.size(); i++)
-            {
-                cvt[row][col] = mp[data[i][index]] % 4;
-                row++;
-            }
-            col++;
-        }
-        return cvt;
+        vector<vector<string>> s = RETRIEVE_DATA(filename);
+        this->Data = Bit_Mask(s);
     }
 };
 
@@ -124,9 +114,8 @@ int main(int argc, char const *argv[])
 {
     // Started 11/2/22
     DecisionTree DT("DS-data.csv");
-    DT.Bit_Mask(DT.Data);
-    vector<vector<int>> ans = DT.Bit_Mask(DT.Data);
-    PrintData<int>(ans);
+    // DT.Bit_Mask(DT.Data);
+    PrintData<int>(DT.Data);
 
     return 0;
 }
