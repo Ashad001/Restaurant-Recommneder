bd6a94c5a2cd1b824b5678ead635ae5d04c7fed5
Ashad001 <93534298+Ashad001@users.noreply.github.com>
2022-11-14 22:21:42 +0500

    masking data
    converted string data to integers

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
 using namespace std;
 
 class TreeNode
 {
 public:
-
     // 1. int column value.. refers to column split on by parent node
     //    null in root's case
     int ColVal;
@@ -24,29 +26,119 @@ public:
     // constructor
     TreeNode()
     {
-        ColVal = ColSplitOn = Guess = NULL;
+        ColVal = ColSplitOn = Guess = 0;
     }
 };
+vector<vector<string>> RETRIEVE_DATA(string filename)
+{
+    vector<vector<string>> record;
+    vector<string> row;
+    string line, data;
+
+    fstream file("DS-data.csv", ios::in);
+    if (file.is_open())
+    {
+        while (getline(file, line))
+        {
+            row.clear();
+
+            stringstream s(line);
+            while (getline(s, data, ','))
+            {
+                row.push_back(data);
+            }
+            record.push_back(row);
+        }
+        // for (int i = 0; i < record.size(); i++)
+        // {
+        //     for (int j = 0; j < record[i].size(); j++)
+        //     {
+        //         cout << record[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+    }
+    else
+    {
+        cout << "Could not Open file";
+    }
+    return record;
+}
+bool alreadyExist(string c, vector<string> ch)
+{
+    for (auto x : ch)
+    {
+        if (x == c)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 class DecisionTree
 {
 public:
     TreeNode *root;
-    vector<vector<int>> Data;
+    vector<vector<string>> Data;
     vector<vector<int>> colVals;
-    DecisionTree()
+    DecisionTree(string filename)
     {
-        // This Constructor Will Fetch The Data -- OVER @HASEEB   
+        // This Constructor Will Fetch The Data -- OVER @HASEEB
+        this->Data = RETRIEVE_DATA(filename);
     }
-    
+    vector<vector<int>> Bit_Mask(vector<vector<string>> &data)
+    {
+        vector<vector<int>> cvt(data.size(), vector<int>(data[0].size() - 2, 0));
+        vector<string> alreadyEx;
+        int index = 2;
+        int row = 0, col = 0;
+        map<string, int> mp;
+
+        for (index = 2; index < data[0].size(); index += 1)
+        {
+            row = 0;
+            alreadyEx.clear();
+            mp.clear();
+            for (int i = 0; i < data.size(); i++)
+            {
+                mp[data[i][index]]++;
+            }
+            for (int i = 0; i < data.size(); i++)
+            {
+                if (alreadyExist(data[i][index], alreadyEx) == 0)
+                {
+                    alreadyEx.push_back(data[i][index]);
+                }
+                cvt[row][col] = mp[data[i][index]] % 4;
+                row++;
+            }
+            col++;
+        }
+        return cvt;
+    }
 };
 
-void Fit(vector<vector<int>> matrix, vector<vector<int>> label)
+template <class T>
+void PrintData(vector<vector<T>> &data)
 {
+    for (auto row : data)
+    {
+        for (auto num : row)
+        {
+            cout << num << ", ";
+        }
+        cout << endl;
+    }
 }
 
 int main(int argc, char const *argv[])
 {
     // Started 11/2/22
+    DecisionTree DT("DS-data.csv");
+    DT.Bit_Mask(DT.Data);
+    vector<vector<int>> ans = DT.Bit_Mask(DT.Data);
+    PrintData<int>(ans);
 
     return 0;
 }
